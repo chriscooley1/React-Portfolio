194623e701d8b7f666fdc1a03bdefa85d910bd25
Chris Cooley <68765420+chriscooley1@users.noreply.github.com>
2021-02-09 19:37:45 -0700

    added phone number, email, github and linkedin links

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -17,14 +17,54 @@ function SearchForm(props) {
           placeholder="Name"
           id="breed"
         />
+        <label htmlFor="breed">Email</label>
+        <input
+          value={props.search}
+          onChange={props.handleInputChange}
+          name="breed"
+          list="breeds"
+          type="text"
+          className="form-control"
+          placeholder="Email"
+          id="breed"
+        />
+        <label htmlFor="breed">Message</label>
+        <input
+          value={props.search}
+          onChange={props.handleInputChange}
+          name="breed"
+          list="breeds"
+          type="text"
+          className="form-control"
+          placeholder="Message"
+          id="breed"
+        />
         <datalist id="breeds">
           {props.breeds.map(breed => (
             <option value={breed} key={breed} />
           ))}
         </datalist>
         <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
-          Search
+          Submit
         </button>
+      </div>
+      <br></br>
+      <div class="col-xl-6 col-sm-12 border-left border-primary mainLinkBox">
+        <div class="container linkBox">
+          <ul>
+            <li class="phone textContact"><i class="fa fa-phone"></i><span> Phone</span><a href="tel:915-246-2751">
+              (915)246-2751</a></li>
+            <li class="email textContact"><i class="fa fa-envelope"></i><span> Email</span><a
+              href="mailto:chriscooley1984@yahoo.com">
+              chriscooley1984@yahoo.com</a></li>
+            <li class="github"><i class="fab fa-github"></i><span> Github</span><a
+              href="https://github.com/chriscooley1">
+              chriscooley1</a></li>
+            <li class="linkedin"><i class="fab fa-linkedin"></i><span> LinkedIn</span><a
+              href="https://www.linkedin.com/in/chris-cooley-65057b14/">
+              chris-cooley-65057b14</a></li>
+          </ul>
+        </div>
       </div>
     </form>
   );
