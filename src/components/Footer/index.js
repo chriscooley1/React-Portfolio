63bf2bd8b6ff81ea9daf45ece09db5d5de5a3af8
Chris Cooley <68765420+chriscooley1@users.noreply.github.com>
2021-02-11 14:09:04 -0700

    added social links to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,66 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './Footer.css';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <footer className="footer">
-      <span>Chris Cooley 2021</span>
-    </footer>
+    <div className='footer-container'>
+      <div className='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div className='footer-link-items'>
+          </div>
+        </div>
+      </div>
+      <section className='social-media'>
+        <div className='social-media-wrap'>
+          <div className='footer-logo'>
+            
+            <Link to='/' className='social-logo'>
+              Contact Me @
+            </Link>
+
+          </div>
+          <div className='social-icons'>
+          {/* eslint-disable-next-line */}
+          <a
+              className='social-icon-link GitHub'
+              href='https://github.com/chriscooley1'
+              target='_blank'
+              aria-label='GitHub'
+            >
+              <i className="fab fa-github" />
+            </a>
+            {/* eslint-disable-next-line */}
+          <a
+              className='social-icon-link LinkedIn'
+              href="https://www.linkedin.com/in/chris-cooley-65057b14/"
+              target='_blank'
+              aria-label='LinkedIn'
+            >
+              <i className='fab fa-linkedin' />
+            </a> 
+            {/* eslint-disable-next-line           */}
+            <a
+              className='social-icon-link facebook'
+              href='https://www.facebook.com/chris.cooley.146/'
+              target='_blank'
+              aria-label='Facebook'
+            >
+              <i className='fab fa-facebook-f' />
+            </a>
+            <a
+              className='social-icon-link'
+              href='/src/images/MyResume.pdf'
+              target='_blank'
+              aria-label='Resume'
+            >
+              <i className="far fa-file" />
+            </a>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
 
