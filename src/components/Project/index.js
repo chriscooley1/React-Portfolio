b2d78faa7fb182d61c6d641c097e201b066b6c09
Chris Cooley <68765420+chriscooley1@users.noreply.github.com>
2021-02-11 14:28:39 -0700

    added about me info

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,10 +1,15 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import '../App.css';
+// import { Button } from './Button';
+import './style.css';
 
-function Project(props) {
+function Project() {
   return (
-    <div className="project text-center" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
-      {props.children}
+    <div className='project-container'>
+      <img src='/images/img-9.jpg' alt=""/>
+      <h1>ABOUT ME</h1>
+      <p>I'm <strong>Chris Cooley</strong>. I'm currently doing a bootcamp through the University of Utah to become a full stack web developer. Throughout my studies, I've been learning HTML, CSS, JavaScript, Node.js, MongDB, React.js and many other languages. I have a Bachelor's Degree in Psychology where I originally planned on becoming a counselor, but later chose to pursue a career with web development. I live in Saint George, UT, where I met my wife, Star. We just bought a house this past year. In my free time, I love playing ultimate frisbee, hiking, running, and bowling. I also love to just relax and watch a good movie.
+      </p>
     </div>
   );
 }
