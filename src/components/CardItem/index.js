06b68423f93786569f80d1234cd2ad0182e405d5
Chris Cooley <68765420+chriscooley1@users.noreply.github.com>
2021-02-11 11:34:55 -0700

    modified links to various projects

--- src/components/CardItem/index.js
+++ src/components/CardItem/index.js
@@ -1,14 +1,26 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function CardBtn(props) {
+function CardItem(props) {
   return (
-    <button 
-      onClick={props.onClick} 
-      className={`card-btn ${props["data-value"]}`} 
-      data-value={props["data-value"]} 
-    />
+    <>
+      <li className='cards__item'>
+        <Link className='cards__item__link' to={props.path}>
+          <figure className='cards__item__pic-wrap' data-category={props.label}>
+            {/* eslint-disable-next-line */}
+            <img
+              className='cards__item__img'
+              alt='Travel Image'
+              src={props.src}
+            />
+          </figure>
+          <div className='cards__item__info'>
+            <h5 className='cards__item__text'>{props.text}</h5>
+          </div>
+        </Link>
+      </li>
+    </>
   );
 }
 
-export default CardBtn;
+export default CardItem;
--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,26 +1,56 @@
-import React from "react";
-import CardBtn from "../CardBtn";
-import "./style.css";
+import React from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
 
-function Card(props) {
+function Cards() {
   return (
-    <div
-      className="card"
-      style={{
-        backgroundImage: props.image ? `url(${props.image})` : "none"
-      }}
-    >
-      {!props.image && <i className="fa fa-spinner fa-spin" aria-hidden="true" />}
-      <CardBtn
-        onClick={props.handleBtnClick}
-        data-value="pass"
-      />
-      <CardBtn
-        onClick={props.handleBtnClick}
-        data-value="pick"
-      />
+    <div className='cards'>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+          
+            <CardItem
+              // href="https://katers0259.github.io/Pick-your-pet/"
+              img src="https://github.com/katers0259/Pick-your-pet"
+              text='Find the perfect pet for you with Pick-your-pet'
+              label='Pets'
+              path="https://katers0259.github.io/Pick-your-pet/"
+            />
+            <CardItem
+              // href="https://fathomless-journey-30707.herokuapp.com/"
+              img src="https://github.com/chriscooley1/Workout-Tracker-HW17"
+              text='Track your daily workouts using this Fitness Tracker'
+              label='Fitness'
+              path="https://fathomless-journey-30707.herokuapp.com/"
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              // href=" https://mysterious-temple-74523.herokuapp.com/"
+              src='https://github.com/chriscooley1/Budget-Tracker-HW18'
+              text='Track your spending habits with this Budget Tracker'
+              label='Budget'
+              path=" https://mysterious-temple-74523.herokuapp.com/"
+            />
+            <CardItem
+              // href="https://polar-thicket-33954.herokuapp.com/"
+              src='https://github.com/chriscooley1/Note-Taker-HW11'
+              text='Write down your thoughts with Note Taker'
+              label='Notes'
+              path="https://polar-thicket-33954.herokuapp.com/"
+            />
+            <CardItem
+              // href="https://pure-mesa-60058.herokuapp.com/"
+              src='https://github.com/johndolacjr/SafelyRent'
+              text='Rate your tenants with Safley Rent'
+              label='Rent'
+              path="https://pure-mesa-60058.herokuapp.com/"
+            />
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Card;
+export default Cards;
