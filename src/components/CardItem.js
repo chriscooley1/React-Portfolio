8ba2b257680d8afe88761f45b086cfa7560465b8
Chris Cooley <68765420+chriscooley1@users.noreply.github.com>
2021-02-13 14:31:35 -0700

    modified readme, added screenshot and added deployment link

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 function CardItem(props) {
   return (
     <>
       <li className='cards__item'>
-        <Link className='cards__item__link' to={props.path}>
+      {/* eslint-disable-next-line */}
+        <a className='cards__item__link' href={props.path} target="_blank">
           <figure className='cards__item__pic-wrap' data-category={props.label}>
-            {/* eslint-disable-next-line */}
+          {/* eslint-disable-next-line */}
             <img
               className='cards__item__img'
-              alt='Travel Image'
               src={props.src}
             />
           </figure>
           <div className='cards__item__info'>
             <h5 className='cards__item__text'>{props.text}</h5>
           </div>
-        </Link>
+        </a>
       </li>
     </>
   );
