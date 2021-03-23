22b36187e1d1c4a2028785897f997fe6b2670ece
Chris Cooley <68765420+chriscooley1@users.noreply.github.com>
2021-03-22 21:16:42 -0600

    modified headline and removed github commit history

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,7 +8,7 @@ function HeroSection() {
     <div className='hero-container'>
       <p>My name is <strong>Chris Cooley</strong>.</p>
       <br></br>
-      <p> I'm currently doing a bootcamp through the University of Utah to become a full stack web developer. Throughout my studies, I've been learning HTML, CSS, JavaScript, Node.js, MongDB, React.js and many other languages. I have a Bachelor's Degree in Psychology where I originally planned on becoming a counselor, but later chose to pursue a career with web development. I live in Saint George, UT, where I met my wife, Star. We just bought a house this past year. In my free time, I love playing ultimate frisbee, hiking, running, and bowling. I also love to just relax and watch a good movie. </p>
+      <p> I recently finished a bootcamp through the University of Utah to become a full stack web developer. Throughout my studies, I've been learning HTML, CSS, JavaScript, Node.js, MongDB, React.js and many other languages. I have a Bachelor's Degree in Psychology where I originally planned on becoming a counselor, but later chose to pursue a career with web development. I live in Saint George, UT, where I met my wife, Star. We just bought a house this past year. In my free time, I love playing ultimate frisbee, hiking, running, and bowling. I also love to just relax and watch a good movie. </p>
     </div>
   );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,14 +2,10 @@ import React from 'react';
 import '../../App.css';
 import Cards from '../Cards';
 import Footer from '../Footer';
-import ReactGhCal from 'react-github-calendar';
 
 export default function Portfolio() {
   return (
   <>
-    <h1> <br></br> GitHub Commit History
-      <ReactGhCal username='chriscooley1' className="githubCommits"/>
-    </h1>
     <Cards/>
     <Footer />
   </>
